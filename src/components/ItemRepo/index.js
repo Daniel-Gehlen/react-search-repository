f323d8cf145415f583f6d30fcccd5c31081df105
Daniel Gehlen <harmonia251251@gmail.com>
2023-11-13 06:14:57 -0300

    build

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -12,8 +12,9 @@ function ItemRepo({repo, handleRemoveRepo}) {
     <ItemContainer onClick={handleRemove}>
         <h3>{repo.name}</h3>
         <p>{repo.full_name}</p>
+
         <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />
-        <a href="#"  rel="noreferrer" className="remover">Remover</a>
+        <button className="button.remover" onClick={() => handleRemove(repo.id) }>Remover</button>
         <hr />
     </ItemContainer>
   )
--- src/components/ItemRepo/styles.js
+++ src/components/ItemRepo/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const ItemContainer = styled.div`
     width: 80%;
@@ -9,18 +9,30 @@ export const ItemContainer = styled.div`
     }
 
     p {
-        font-size:16px;
+        font-size: 16px;
         color: #FAFAFA60;
-        margin-bottom:20px;
+        margin-bottom: 20px;
     }
 
     a.remover {
         color: #FF0000;
-        margin-top:20px;
+        margin-top: 20px;
     }
 
     hr {
         color: #FAFAFA60;
         margin: 20px 0;
+    }
+
+    button.remover {
+        background-color: transparent;
+        border: none;
+        color: #FAFAFA60;
+        cursor: pointer;
+        text-decoration: underline;
+      }
+      
+    button.remover:hover {
+        color: #ff6666;
     }
 `
